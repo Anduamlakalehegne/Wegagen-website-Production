f204ea24bd4d16a1b3cc8ec9e25d96ecabb24965
AbdulqaderAhmed <abdulqaderahmed32@gmail.com>
2024-09-09 16:09:26 +0300

    table css, flag and search fixed for rate history

--- src/Components/Pages/RateHistory/RateHistory.jsx
+++ src/Components/Pages/RateHistory/RateHistory.jsx
@@ -2,10 +2,15 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import styles from "../Home/home.module.css";
 import { format, parseISO } from "date-fns";
+import StikyNav from "../Common/StikyNav";
+import Mega_Menu from "../Common/Mega_Menu";
+import About_us from "../../../assets/Images/common.jpg";
+import Footer from "../Common/Footer";
 
 export default function RateHistory() {
   const [rateData, setRateData] = useState(null);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
   const [selectDate, setSelectDate] = useState({
     date1: "",
     date2: "",
@@ -16,6 +21,8 @@ export default function RateHistory() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    setLoading(true);
+
     try {
       const res = await axios(
         `http://localhost:8000/api/exchange-rate-history/search?date1=${selectDate.date1}&date2=${selectDate.date2}`
@@ -23,19 +30,21 @@ export default function RateHistory() {
 
       if (res) {
         setRateData(res.data);
-        const date = parseISO(res?.data[0].attributes.date);
+        const date = parseISO(res?.data[0].created_at);
         setFormattedDate(format(date, "MMM dd, yyyy"));
         setFormattedDate2(format(date, "EEEE MMM dd, yyyy"));
         setError(null);
       }
     } catch (error) {
-      console.log(error.response ? error.response.data : error.message);
       setError(error.response ? error.response.data : error.message);
+    } finally {
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     const fetchRate = async () => {
+      setLoading(true);
       try {
         const response = await axios(
           "http://localhost:8000/api/exchange-rate-history"
@@ -49,34 +58,65 @@ export default function RateHistory() {
           setFormattedDate2(format(date, "EEEE MMM dd, yyyy"));
         }
       } catch (error) {
-        console.log(error);
+        setError(error.response ? error.response.data : error.message);
+      } finally {
+        setLoading(false);
       }
     };
     fetchRate();
   }, []);
 
   return (
-    <div className="conatiner">
-      <h1>History</h1>
+    <>
+      <StikyNav />
+      <Mega_Menu />
 
-      <div className="mx-auto flex flex-col gap-20">
-        <form onSubmit={handleSubmit} className="flex mx-auto gap-10 text-lg">
-          <div className="flex gap-10">
-            <div className="flex gap-3">
-              <label htmlFor="date-one" className="mt-2">
-                Date:
-              </label>
-              <input
-                type="date"
-                id="date1"
-                name="date1"
-                className="border-2 border-black rounded-lg p-1"
-                onChange={(e) =>
-                  setSelectDate({ ...selectDate, date1: e.target.value })
-                }
-              />
+      <div className={styles.agarImage}>
+        <img src={About_us} alt="About Us" />
+      </div>
+
+      <div style={{ marginBottom: "100px" }}>
+        <div className={styles.exchangeRateHistory}>
+          <div className={styles.exchangeRateInfo}>
+            <div className={styles.rightSide}>
+              <p className={styles.headerTitle3}>Exchange Rate History</p>
+              {/* <p className={styles.headerTitle2}>የዕለቱ የዉጭ ምንዛሬ ተመን</p> */}
+              <p className={styles.headerTitle2}>
+                Foreign Exchange Rate Applicable
+              </p>
+              <p
+                style={{
+                  color: "#ff6b0b",
+                  fontSize: "23px",
+                  textAlign: "center",
+                }}
+              >
+                On {formattedDate2}
+              </p>
             </div>
-            {/* <div className="flex gap-3">
+          </div>
+
+          <div className="mx-auto my-10">
+            <form
+              onSubmit={handleSubmit}
+              className="flex mx-auto gap-10 text-lg"
+            >
+              <div className="flex gap-10">
+                <div className="flex gap-3">
+                  <label htmlFor="date-one" className="mt-2">
+                    Select date:
+                  </label>
+                  <input
+                    type="date"
+                    id="date1"
+                    name="date1"
+                    className="border-2 border-black rounded-lg p-1"
+                    onChange={(e) =>
+                      setSelectDate({ ...selectDate, date1: e.target.value })
+                    }
+                  />
+                </div>
+                {/* <div className="flex gap-3">
               <label htmlFor="date-two" className="mt-2">
                 To:
               </label>
@@ -89,37 +129,26 @@ export default function RateHistory() {
                   setSelectDate({ ...selectDate, date2: e.target.value })
                 }
               />
-            </div> */}
-          </div>
-          <button type="submit" className="bg-black text-white p-2 rounded-lg">
-            Search
-          </button>
-        </form>
-
-        {error && <p className="text-red-500">{error?.message}</p>}
-
-        <div style={{ marginBottom: "100px" }}>
-          <div className={styles.exchangeRate}>
-            <div className={styles.exchangeRateInfo}>
-              <div className={styles.rightSide}>
-                <p className={styles.headerTitle3}>Exchange Rate</p>
-                <p className={styles.headerTitle2}>የዕለቱ የዉጭ ምንዛሬ ተመን</p>
-                <p style={{ color: "#ff6b0b", fontSize: "17px" }}>
-                  Foreign Exchange Rate Applicable
-                </p>
-                <p style={{ color: "#ff6b0b", fontSize: "17px" }}>
-                  On {formattedDate2}
-                </p>
+                </div> */}
               </div>
-            </div>
-            <div className={styles.ecxhangerightImg}>
-              <div className={styles.popOuter}>
-                <div
-                  className={styles.popexchange}
-                  style={{ marginBottom: "10px" }}
-                ></div>
+              <button
+                type="submit"
+                className="bg-orange-400 text-white p-2 rounded-lg hover:bg-black"
+              >
+                Search
+              </button>
+            </form>
 
-                {/* table data */}
+            {error && (
+              <p className="text-red-500 text-2xl mt-10">{error?.message}</p>
+            )}
+          </div>
+          <div className={styles.ecxhangerightImg}>
+            <div className={styles.popOuter}>
+              <div
+                className={styles.popexchange}
+                style={{ marginBottom: "10px" }}
+              >
                 <table>
                   <thead>
                     <th></th>
@@ -144,6 +173,7 @@ export default function RateHistory() {
                     </tr>
                   </thead>
                   <tbody>
+                    {loading && <h3>Loading data...</h3>}
                     {rateData &&
                       rateData.map((rate, index) => (
                         <tr
@@ -151,11 +181,10 @@ export default function RateHistory() {
                           className={index % 2 === 1 ? styles.active : ""}
                         >
                           <td>
-                            {/* <img
-                    src={`https://weg.back.strapi.wegagen.com${rate.attributes.flag.data[0].attributes.url}`}
-                    alt={rate.attributes.code}
-                  /> */}
-                            image
+                            <img
+                              src={`https://weg.back.strapi.wegagen.com${rate.flag}`}
+                              alt={rate.code}
+                            />
                           </td>
                           <td>{rate.code}</td>
                           <td>{rate.unit}</td>
@@ -172,6 +201,8 @@ export default function RateHistory() {
           </div>
         </div>
       </div>
-    </div>
+
+      <Footer />
+    </>
   );
 }
